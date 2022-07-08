99008635f9fbc2e99cd8d0fdbf9bc09000ddd506
Nikita Balenko <nikitabalenko2013@gmail.com>
2022-07-08 19:53:23 +0300

    дз к 4 семинару

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int power(int A, int B)
+{
+    int i = 1;
+    int Res = A;
+    while (i < B)
+    {
+        Res = Res * A;
+        i++;
+    }
+    return Res;
+}
+System.Console.WriteLine(power(3, 5));
---
+++ Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+System.Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] arr = new int[size];
+
+int[] createArr(int a)
+{
+    int i = 0;
+    while (i < a)
+    {
+        System.Console.Write($"Введите {i + 1} элемент массива: ");
+        arr[i] = int.Parse(Console.ReadLine()!);
+        i++;
+    }
+    return arr;
+}
+
+void printArr()
+{
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i+1 < size) System.Console.Write(", ");
+    }
+}
+
+
+createArr(size);
+printArr();
